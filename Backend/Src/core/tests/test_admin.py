d20ddfd00e1c761e5beeb9f412169bddff4e0898
Benedikt Óskarsson <benedikto16@ru.is>
2019-04-17 23:09:41 -0400

    Created the create user page for admin

--- Backend/Src/core/tests/test_admin.py
+++ Backend/Src/core/tests/test_admin.py
@@ -25,3 +25,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.username)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
