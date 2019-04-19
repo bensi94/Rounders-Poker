232e2f163578c48d74986a6da96d60c4938a5f9e
Benedikt Óskarsson <benedikto16@ru.is>
2019-04-19 01:12:00 -0400

    Created management command for django wait_for_db
    This command makes sure that the backend application will wait for
    the database to be ready before running the django build.
    Changed docker-compose file to use this command and also added
    migrate command to the docker-compose file when the application is
    started. The command is tested in test_commands.py

--- Backend/Src/backend/settings.py
+++ Backend/Src/backend/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST),
+        'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
