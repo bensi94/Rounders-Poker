4f2476325feef3dd2d9f0815a010761ee465eb24
Benedikt Óskarsson <benedikto16@ru.is>
2019-04-20 00:50:15 -0400

    Added Crate user endpoint to backend
    * Added a path to the api at api/user to urls
    * Created a serializer for the Create user endpoint, checks that
      fields are: username, password and name. Checks that password is
      write_only and longer than 5 characters
    * Added the create pattern to user urls
    * Added the CreateUserView from the generics rest_framework,
      CrateAPIView
    * Created tests for the create user endpoint
    * Fixed typo in settnings.py

---
+++ Backend/Src/user/tests/test_user_api.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'username': 'bensi94',
+            'password': 'testpass',
+            'name': 'Benedikt Oskarsson'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """Test creating a user that already exists"""
+        payload = {'username': 'bensi94', 'password': 'testpass'}
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that the password must be more than 5 characters"""
+        payload = {'username': 'bensi94', 'password': 'pw'}
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            username=payload['username']
+        ).exists()
+        self.assertFalse(user_exists)
