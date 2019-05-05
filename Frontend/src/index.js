65b5cb442b1fc1f4faa0bb66cca7ce576ddd1117
Benedikt Óskarsson <benedikto16@ru.is>
2019-05-05 12:00:23 -0400

    Setup redux with devtools, middleware and a root reducer
    Also changed docker-compose file to use environment varibles
    Created a root reducer at reducers/index.js
    Created store at store/index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,16 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'element-theme-default';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // Project files
 import './styles/main.less';
 import Header from './components/Header';
+import store from './store';
 
 const App = () => {
     return (
-        <Router>
-            <Header />
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Header />
+            </Router>
+        </Provider>
     );
 };
 
